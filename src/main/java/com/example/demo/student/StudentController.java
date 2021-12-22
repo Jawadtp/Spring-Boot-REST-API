e1c2522ff661226295d7d8ef88ed161dcf2ab50a
Jawadtp <jawadtpmohd@gmail.com>
2021-12-22 23:34:59 +0530

    Database properties defined

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -5,18 +5,30 @@ import java.util.List;
 import java.time.LocalDate;
 import java.time.Month;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path="api/v1/student")
+
 public class StudentController 
 {
+
+	private final StudentService studentService;
+
+	@Autowired
+	public StudentController(StudentService studentService)
+	{
+		this.studentService = studentService;
+	}
+	
     @GetMapping
 	public List<Student> getStudents()
 	{
-		return List.of(new Student(1L, "Mohammed Jawad TP", "jawadtpmohd@gmail.com", LocalDate.of(2000, Month.APRIL, 10), 21));
+		return studentService.getStudents();
+	//	return List.of(new Student(1L, "Mohammed Jawad TP", "jawadtpmohd@gmail.com", LocalDate.of(2000, Month.APRIL, 10), 21));
 	}
 
     
